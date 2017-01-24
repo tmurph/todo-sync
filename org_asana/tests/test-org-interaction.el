b1a6850cfc76239a6a55df0b1ed8b4faa9e250b1
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-01-24 10:34:38 -0800

    Make a few helper functions to ease writing tests.

--- org_asana/tests/test-org-interaction.el
+++ org_asana/tests/test-org-interaction.el
@@ -7,6 +7,33 @@
 (require 'ert)
 (require 'org-interaction)
 
+(defun oi-make-headline-from-text (text)
+  "Create a headline from the Org-formatted TEXT."
+  (let (org-mode-hook)
+    (org-element-map (with-temp-buffer
+                       (org-mode)
+                       (insert text)
+                       (org-element-parse-buffer))
+        'headline #'identity nil t)))
+
+(defun oi-concat-with-newlines (&rest args)
+  "Concatenate ARGS with newlines in between."
+  (mapconcat #'identity args "\n"))
+
+(defun oi-make-drawer-text-from-plist (plist)
+  "Create Org property drawer text with keys & values from PLIST."
+  (let (k v retval)
+    (push ":PROPERTIES:" retval)
+    (while plist
+      (setq k (pop plist) v (pop plist))
+      (push (concat (upcase (symbol-name k)) ":"
+                    (make-string (max 1 (- 10 (length (symbol-name k))))
+                                 ? )
+                    v)
+            retval))
+    (push ":END:" retval)
+    (apply 'oi-concat-with-newlines (nreverse retval))))
+
 
 (provide 'test-org-interaction)
 ;;; test-org-interaction.el ends here
