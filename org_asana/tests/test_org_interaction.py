2dfb9ff856c278e97b449d5f1c341ed44165e1dc
Trevor Murphy <trevor.m.murphy@gmail.com>
2017-01-25 11:56:25 -0800

    Use a unit test to define an OrgNode.

--- org_asana/tests/test_org_interaction.py
+++ org_asana/tests/test_org_interaction.py
@@ -1,2 +1,14 @@
 import pytest
 import pytest_mock
+
+from org_asana.node import Node, trees_equal_p
+from org_asana.org_interaction import OrgNode
+
+@pytest.mark.parametrize("org_node, expected_node", [
+    (OrgNode.from_dict({'id': 1, 'parent': None}),
+     Node.from_dict({'id': 1, 'parent': None,
+                     'title': None, 'paragraph': None}))
+])
+def test_org_node(org_node, expected_node):
+    "Is an OrgNode just a Node with extra attributes?"
+    assert trees_equal_p(org_node, expected_node)
